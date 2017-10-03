a423f40eebc86a3388b50ba5cac3450c4c7f50ce
NielsVanM <niels.van.marion@quicknet.nl>
2017-10-03 14:06:16 +0200

    Started working on Landing Page

--- Website/LandingPage/views.py
+++ Website/LandingPage/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("501 Not Implemented ( Yet )")
+    return render(request, 'LandingPage/index.html')
