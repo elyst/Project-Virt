74a732e952b1d4fb32ce09e9455377cf2afbe2ae
elyst <johnverdonck@hotmail.com>
2017-10-10 00:08:42 +0200

    changed logout path

--- Website/Login/urls.py
+++ Website/Login/urls.py
@@ -7,6 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.custom_login, name='login'),
     url(r'^register/$', views.signUpRequest, name='signUpRequest'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout')
     
 ]
