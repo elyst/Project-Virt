b9f431a84aa22523de3a4b6c74f1b99dd4be395e
NielsVanM <niels.van.marion@quicknet.nl>
2017-10-14 15:03:31 +0200

    Created functions which creates VM's

--- Website/VMManager/models.py
+++ Website/VMManager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+class VirtualMachine(models.Model):
+    VMID = models.CharField(max_length=255)
+    Name = models.CharField(max_length=255)
+    CPUCores = models.IntegerField()
+    RAMAmount = models.IntegerField()
+    DISKSize = models.IntegerField()
+
+admin.site.register(VirtualMachine)
