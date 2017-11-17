a62db07535df353d5f0a5462735b839610182377
Jurre Wolff <jurrewolff@hotmail.com>
2017-11-17 12:57:27 +0100

    Update van laatste code om XML te veranderen. Nu moet de code alleen nog worden geintegreerd in Niels zijn Djangocode.

--- kvm/alterxml.py
+++ kvm/alterxml.py
@@ -1,29 +1,55 @@
 # Import Elementtree(ET) --> ET is used for reading and editing xml files
 import xml.etree.ElementTree as ET
+import uuid
+
+# XML file to parse
+xml = 'kvm.xml'
+
+# Variables from form
+name = 'NoSQL'
+ram = '524288'                           # In Kilobytes
+cores = '2.5'
+os = 'ubuntu'                               # Has to match the names that are in the iso folder!
 
 # Parse XML file
-tree = ET.parse('kvm.xml')
+tree = ET.parse(str(xml))
 
-# Find desired tag to edit
-root = tree.find('devices')
-root2 = root.find('interface')
-root3 = root2.find('source')
+# Indexing of all necessary parents
+root2 = tree.find('devices')
+root3 = root2.find('interface')
+root4 = root3.find('source')
+
+# Change VM name
+nameroot = tree.find('name')
+nameroot.text = name
+
+# Change uuid
+uuid = uuid.uuid4()                         # Generate random uuid
+uuidroot = tree.find('uuid')
+uuidroot.text = str(uuid)
 
 # Change mem size
-memroot1 = tree.find('memory')
-memroot1.text = '1024'
-memroot2 = tree.find('currentMemory')
-memroot2.text = '1024'
+mroot = tree.find('memory')
+mroot.text = memory
+mroot2 = tree.find('currentMemory')
+mroot2.text = ram
 
 # Change number of vcpu's
-vcpuroot = tree.find('vcpu')
-vcpuroot.text = '2'
+vcpuroot = tree.find('vcpu')                # Tree.find has to be used here bcause there's no parent? 
+vcpuroot.text = cores
 
-# Change iso file
-isoroot = root.find('')
+# Change disk image name accordingly
+imagepath = '/home/jurrewolff/Desktop/git/Project-Virt/kvm/images/disk.img'
+imagepath = imagepath.replace('disk', str(nameroot.text))
+root2[1][1].set('file', str(imagepath))
+
+# Change iso file (This one is the right way)
+isopath = '/home/jurrewolff/Desktop/git/Project-Virt/kvm/iso/os.iso'
+isopath = isopath.replace('os', str(os))
+root2[2][1].set('file', str(isopath))
 
 # Change value of network interface
-alter = root3.set('bridge', 'br0')
+root4.set('bridge', 'br0')                  # Bridges have to be automized!
 
 # Write changes to XML file
 tree.write('kvm.xml')
--- kvm/dinges.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('kvmtrois.xml')
-doc = tree.getroot()
-
-change = doc.find('devices')
-
-tree.write('kvmtrois.xml')
-print(change)
