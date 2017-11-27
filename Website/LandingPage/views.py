5d8078ae9d3d28221a79e1d34ffbbb9a07e83f9e
elyst <john@johnny.ddnsking.com>
2017-11-27 16:14:12 +0100

    First commit

--- Website/LandingPage/views.py
+++ Website/LandingPage/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'LandingPage/index.html')
+
+def contact(request):
+    return render(request, 'LandingPage/page2.html')
+
+def pricing(request):
+    return render(request, 'LandingPage/page1.html')
+
